00b9e401bef57a1d0e379fa5abe6ca01ebf6c8de
DamnBoy420 <sander.henricsson@elev.ga.lbs.se>
2025-09-22 11:11:14 +0200

    Layers and Player Rotation

--- TDS/Assets/Scripts/Player.cs
+++ TDS/Assets/Scripts/Player.cs
@@ -6,8 +6,12 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     Vector2 moveInput;
     [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float rotationSpeed = 700f;
     [SerializeField] float bulletSpeed = 7f;
     [SerializeField] GameObject bullet;
+    [SerializeField] GameObject gun;
+
+    float targetAngle;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,7 +27,7 @@ public class Player : MonoBehaviour
 
     void OnAttack()
     {
-        Rigidbody2D playerBullet =  Instantiate(bullet, transform.position, Quaternion.identity).GetComponent<Rigidbody2D>();
+        Rigidbody2D playerBullet =  Instantiate(bullet, gun.transform.position, transform.rotation).GetComponent<Rigidbody2D>();
         playerBullet.AddForce(transform.up * bulletSpeed, ForceMode2D.Impulse);
     }
 
@@ -32,5 +36,19 @@ public class Player : MonoBehaviour
     void Update()
     {
         rb.linearVelocity = moveInput * moveSpeed;
+        if (moveInput != Vector2.zero)
+        {
+            targetAngle = Mathf.Atan2(moveInput.y, moveInput.x) * Mathf.Rad2Deg;
+        }
+    }
+    void FixedUpdate()
+    {
+        float rotation = Mathf.MoveTowardsAngle(rb.rotation, targetAngle - 90, rotationSpeed * Time.fixedDeltaTime);
+        rb.MoveRotation(rotation);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
     }
 }
